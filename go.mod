e024f8ba1e951436ec006e57a4f91eab6fbded2b
Saifur Rahman <saifurrahmankhankhan@gmail.com>
2021-08-16 12:11:01 +0530

    created server and implemented GetUserByName method

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Saifu0/orchestrator-service
 go 1.16
 
 require (
+	github.com/hashicorp/go-hclog v0.16.2
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
---
+++ server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"context"
+
+	protos "github.com/Saifu0/orchestrator-service/protos/orchestrator"
+	"github.com/hashicorp/go-hclog"
+)
+
+type Orchestrator struct {
+	log hclog.Logger
+}
+
+func NewOrchestrator(l hclog.Logger) *Orchestrator {
+	oc := &Orchestrator{log: l}
+	return oc
+}
+
+func (oc *Orchestrator) GetUserByName(ctx context.Context, r *protos.Request) (*protos.Response, error) {
+	oc.log.Info("Handle request for GetUserByName, name: ", r.GetName())
+
+	return &protos.Response{Name: r.GetName(), Class: "CS", Roll: 176231}, nil
+}
