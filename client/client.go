5166ee7613717bb5a224af3a5a6d5095f9756bbd
Saifur Rahman <saifurrahmankhankhan@gmail.com>
2021-08-16 12:39:13 +0530

    moved server implementation into server directory and implemented client for request

---
+++ client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	protos "github.com/Saifu0/orchestrator-service/protos/orchestrator"
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+)
+
+var (
+	serverAddr = flag.String("server_addr", "localhost:8080", "The server address in the format of host:port")
+)
+
+func main() {
+	log := hclog.Default()
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Error("Unable in connection with server!", "error", err)
+		os.Exit(1)
+	}
+
+	client := protos.NewOrchestratorClient(conn)
+
+	resp, err := client.GetUserByName(context.Background(), &protos.Request{Name: "Saifurrahman"})
+	if err != nil {
+		log.Error("Unable to call service method, GetUserByName, ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
+}
--- server/server.go
+++ server/server.go
@@ -1,10 +1,15 @@
-package server
+package main
 
 import (
 	"context"
+	"net"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	protos "github.com/Saifu0/orchestrator-service/protos/orchestrator"
-	"github.com/hashicorp/go-hclog"
 )
 
 type Orchestrator struct {
@@ -20,4 +25,22 @@ func (oc *Orchestrator) GetUserByName(ctx context.Context, r *protos.Request) (*
 	oc.log.Info("Handle request for GetUserByName, name: ", r.GetName())
 
 	return &protos.Response{Name: r.GetName(), Class: "CS", Roll: 176231}, nil
+}
+
+func main() {
+	log := hclog.Default()
+
+	gs := grpc.NewServer()
+	oc := NewOrchestrator(log)
+
+	protos.RegisterOrchestratorServer(gs, oc)
+	reflection.Register(gs)
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Error("Unable to create listener", "error", err)
+		os.Exit(1)
+	}
+
+	gs.Serve(listener)
 }
